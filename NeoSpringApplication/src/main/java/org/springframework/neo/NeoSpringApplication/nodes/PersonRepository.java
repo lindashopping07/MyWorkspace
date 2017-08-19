f00eb073363f5d9f0eca67f8491e7059c783463a
Linda <lindashopping07@gmail.com>
2017-08-19 19:54:56 +0530

    Test Application - Initial Commit

---
+++ NeoSpringApplication/src/main/java/org/springframework/neo/NeoSpringApplication/nodes/PersonRepository.java
@@ -0,0 +1,8 @@
+package org.springframework.neo.NeoSpringApplication.nodes;
+
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+public interface PersonRepository extends GraphRepository<Person> {
+
+	Person findByName(String name);
+}
